e74f8cea5dc29e04359b1c323c0198dd29912743
julian <estrotococoa@gmail.com>
2020-09-28 17:55:03 -0300

    validating pais query at template html

--- formularios/src/app/pages/template/template.component.ts
+++ formularios/src/app/pages/template/template.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { PaisService } from 'src/app/services/pais.service';
 
 @Component({
   selector: 'app-template',
@@ -11,11 +12,24 @@ export class TemplateComponent implements OnInit {
   usuario = {
     nombre: '',
     apellido: '',
-    correo: ''
+    correo: '',
+    pais: ''
   }
-  constructor() { }
+
+  paises: any[] = [];
+
+  constructor( private paisService: PaisService ) { }
 
   ngOnInit(): void {
+    this.paisService.getPaises()
+      .subscribe( paises => {
+        this.paises = paises;    
+        
+        this.paises.unshift({
+          nombre: 'Seleccione su país',
+          codigo: ''
+        })
+      })
   }
 
   guardar( forma: NgForm ){ 
--- formularios/src/app/services/pais.service.ts
+++ formularios/src/app/services/pais.service.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { map } from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +10,20 @@ export class PaisService {
   constructor( private http: HttpClient ) { }
 
   getPaises(){
-    return this.http.get('https://restcountries.eu/rest/v2/lang/es');
+
+    return this.http.get('https://restcountries.eu/rest/v2/lang/es')
+      .pipe( 
+        map( (resp: any[]) => {
+          return resp.map( pais => ({ nombre: pais.name, codigo: pais.alpha3Code })
+            
+          //Codigo alternativo
+            // return {
+            //   nombre: pais.name,
+            //   codigo: pais.alpha3Code
+            // }            
+          //})
+          )
+        })
+        );
   }
 }
